a7db140cf9292dce98341cc9ab444003846f7dff
Geoff Russell <Geoff.R.Russell@gmail.com>
2015-03-03 16:50:12 -0800

    Added date picker

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 *
 * Description
 */
-angular.module('myApp', ['chart.js'])
+angular.module('myApp', ['chart.js', 'ui.bootstrap'])
 	.controller('myController', ['$scope', '$http', function($scope, $http){
 		$scope.button = {
 			title:"Press me"
@@ -13,6 +13,41 @@ angular.module('myApp', ['chart.js'])
 		$scope.data = [
 		   [65, 59, 80, 81, 56, 55, 40]
 		];
+		
+		$scope.today = function() {
+		  $scope.dt = new Date();
+		};
+		$scope.today();
+
+		$scope.clear = function () {
+		  $scope.dt = null;
+		};
+
+		// Disable weekend selection
+		$scope.disabled = function(date, mode) {
+		  // return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+			return false;
+		};
+
+		$scope.toggleMin = function() {
+		  $scope.minDate = $scope.minDate ? null : new Date();
+		};
+		$scope.toggleMin();
+
+		$scope.open = function($event) {
+		  $event.preventDefault();
+		  $event.stopPropagation();
+
+		  $scope.opened = true;
+		};
+
+		$scope.dateOptions = {
+		  formatYear: 'yy',
+		  startingDay: 1
+		};
+
+		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+		$scope.format = $scope.formats[0];
 
 		$scope.onClick = function (points, evt) {
 		   console.log(points, evt);
